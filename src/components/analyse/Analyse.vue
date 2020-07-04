b7643b20ee3b4c0477d565137c33a9ae402a59d4
WIZeaz <764696642@qq.com>
2020-07-05 01:30:54 +0800

    基本功能和框架
    用折线图展示病人数量变化

--- src/components/analyse/Analyse.vue
+++ src/components/analyse/Analyse.vue
@@ -1,18 +1,79 @@
 <template>
-    <div>
-        Analyse
-    </div>
+    <div id="myChart" class="chart" :style="{width: '100%', height: '500px'}"></div>
 </template>
 
 <script>
-    export default{
-        data(){
+    import echarts from 'echarts'
+    export default {
+        name: 'xwPassengerFlow',
+        data() {
             return {
-                page:"analyse"
+                xAxisData: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'], // x轴数据，可根据需求
+                yAxisData1: [120, 132, 101, 134, 90, 230, 210], // 数据1
+                yAxisData2: [220, 182, 191, 234, 290, 330, 310], // 数据2
+                yAxisData3: [150, 232, 201, 154, 190, 330, 410], // 数据3
+                yAxisData4: [320, 332, 301, 334, 390, 330, 320], // 数据4
             }
+        },
+        mounted() {
+            this.loadLine();
+        },
+        methods: {
+            loadLine() {
+                let option = {
+                    title: {
+                        text: '本周各类病人数量折线变化图'
+                    },
+                    tooltip: {
+                        trigger: 'axis'
+                    },
+                    legend: {
+                        data: ['新冠', '肺结核', '非典', '鼠疫']
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: {
+                        type: 'category',
+                        boundaryGap: false,
+                        data: this.xAxisData // x轴数据
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    series: [
+                        {
+                            name: '新冠',
+                            type: 'line',
+                            stack: '总量',
+                            data: this.yAxisData1 // y轴数据1
+                        },
+                        {
+                            name: '肺结核',
+                            type: 'line',
+                            stack: '总量',
+                            data: this.yAxisData2 // y轴数据2
+                        },
+                        {
+                            name: '非典',
+                            type: 'line',
+                            stack: '总量',
+                            data: this.yAxisData3 // y轴数据3
+                        },
+                        {
+                            name: '鼠疫',
+                            type: 'line',
+                            stack: '总量',
+                            data: this.yAxisData4 // y轴数据5
+                        }
+                    ]
+                }
+                this.myChartOne = echarts.init(document.getElementById('myChart'))
+                this.myChartOne.setOption(option)
+            },
         }
     }
 </script>
-
-<style>
-</style>
--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,10 +1,43 @@
 <template>
-    <div>
-        Dashboard
+    <div style="margin-top: 60px">
+        <schart :canvasId="canvasId"
+                :type="type"
+                :width="width"
+                :height="height"
+                :data="data"
+                :options="options">
+        </schart>
     </div>
 </template>
 
 <script>
+    import Schart from 'vue-schart';
+    export default {
+        name: "Analyse",
+        data() {
+            return {
+                canvasId: 'myChart',
+                type: 'pie',
+                width: 600,
+                height: 500,
+                data: [
+                    {name: '新冠', value: 1234},
+                    {name: '肺结核', value: 2345},
+                    {name: '非典', value: 2456},
+                    {name: '鼠疫', value: 333},
+                    ],
+                options: {
+                    title: '病人数量占比仪表盘',
+                    bgColor: '#829dca',
+                    titleColor: '#ffffff',
+                    legendColor: '#ffffff'
+                    }
+                }
+            },
+        components:{
+                Schart
+            }
+    };
 </script>
 
 <style>
